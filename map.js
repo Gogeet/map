36360462d50bd84e22968f0cd623832bac773368
Gogeet <133143426+Gogeet@users.noreply.github.com>
2023-05-15 11:16:18 +0200

    Add files via upload

--- map.js
+++ map.js
@@ -15,12 +15,23 @@ function pixelToLatLng(pixelCoordinates) {
 }
 
 // Set the initial view based on the image dimensions
-var southWest = map.unproject([0, imageHeight], map.getMaxZoom());
-var northEast = map.unproject([imageWidth, 0], map.getMaxZoom());
+var southWest = map.unproject([0, imageHeight]);
+var northEast = map.unproject([imageWidth, 0]);
 var imageBounds = L.latLngBounds(southWest, northEast);
 
 // Add the image overlay
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 // Set the map center and zoom level
-map.setView([0, 0], 1);
+map.setView([-800, 800], 0);
+
+
+function pixelToLatLng(pixelCoordinates) {
+    var zoom = map.getZoom();
+    return map.unproject(pixelCoordinates, zoom);
+  }
+  
+  // Example usage: Add a marker at pixel coordinates (800, 800)
+  var pixelCoordinates = [800, 800];
+  var latLng = pixelToLatLng(pixelCoordinates);
+  L.marker(latLng).addTo(map);
