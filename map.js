c035aed108ec158ae2843f7f93c9215af8b91f40
Gogeet <133143426+Gogeet@users.noreply.github.com>
2023-05-15 11:08:55 +0200

    Add files via upload

--- map.js
+++ map.js
@@ -1,10 +1,26 @@
 var imageUrl = 'map.png';
+var imageWidth = 1600;
+var imageHeight = 1600;
 
-var viewer = OpenSeadragon({
-  id: 'map',
-  prefixUrl: 'openseadragon/images/',
-  tileSources: imageUrl,
-  defaultZoomLevel: 0,
-  minZoomLevel: 0,
-  maxZoomLevel: 10,
+var map = L.map('map', {
+  crs: L.CRS.Simple,
+  minZoom: -Infinity,
+  maxZoom: Infinity
 });
+
+// Convert pixel coordinates to geographic coordinates
+function pixelToLatLng(pixelCoordinates) {
+  var zoom = map.getZoom();
+  return map.unproject(pixelCoordinates, zoom);
+}
+
+// Set the initial view based on the image dimensions
+var southWest = map.unproject([0, imageHeight], map.getMaxZoom());
+var northEast = map.unproject([imageWidth, 0], map.getMaxZoom());
+var imageBounds = L.latLngBounds(southWest, northEast);
+
+// Add the image overlay
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+// Set the map center and zoom level
+map.setView([0, 0], 1);
