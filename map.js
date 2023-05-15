09be5b3c9d4f692f7b7be611e7b1c7260ab04fb3
Gogeet <133143426+Gogeet@users.noreply.github.com>
2023-05-15 12:07:48 +0200

    Add files via upload

--- map.js
+++ map.js
@@ -8,21 +8,17 @@ var map = L.map('map', {
   maxZoom: Infinity
 });
 
-// Convert pixel coordinates to geographic coordinates
 function pixelToLatLng(pixelCoordinates) {
   var zoom = map.getZoom();
   return map.unproject(pixelCoordinates, zoom);
 }
 
-// Set the initial view based on the image dimensions
 var southWest = map.unproject([0, imageHeight]);
 var northEast = map.unproject([imageWidth, 0]);
 var imageBounds = L.latLngBounds(southWest, northEast);
 
-// Add the image overlay
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-// Set the map center and zoom level
 map.setView([-800, 800], 0);
 
 
@@ -30,8 +26,39 @@ function pixelToLatLng(pixelCoordinates) {
     var zoom = map.getZoom();
     return map.unproject(pixelCoordinates, zoom);
   }
-  
-  // Example usage: Add a marker at pixel coordinates (800, 800)
-  var pixelCoordinates = [800, 800];
-  var latLng = pixelToLatLng(pixelCoordinates);
-  L.marker(latLng).addTo(map);
+
+//Structure: [x, y, type, Planet, System, Sector, link to WookLegends, link to WookCanon, Name to show]
+var pointsOfInterest = [
+    [800, 800, , , , , , , "Galactic Centre"],
+    [767, 670, "Planet", "Coruscant", "Coruscant", "Corusca", "https://starwars.fandom.com/wiki/Coruscant/Legends", "https://starwars.fandom.com/wiki/Coruscant", "Coruscant"]
+]
+
+var structure = ["Body: ", "Name: ", "System: ", "Sector: ", "Wook Legends", "Wook Canon"];
+
+for(let i = 0; i < pointsOfInterest.length; i++) {
+    var pixelCoordinates = [pointsOfInterest[i][0], pointsOfInterest[i][1]];
+    var latLng = pixelToLatLng(pixelCoordinates);
+
+    var marker = L.marker(latLng);
+
+    var text = "<b>" + pointsOfInterest[i][8] + "</b>";
+
+    marker.bindTooltip(text);
+
+    for(let j = 2; j < pointsOfInterest[i].length-3; j++) {
+        if(pointsOfInterest[i][j] !== undefined) {
+            text += "<br />" + structure[j-2] + pointsOfInterest[i][j];
+        }
+    }
+
+    for(let j = pointsOfInterest[i].length-3; j < pointsOfInterest[i].length-1; j++) {
+        if(pointsOfInterest[i][j] !== undefined) {
+            text += "<br /> <a href=\"" + pointsOfInterest[i][j] + "\" target=\"_blank\">" + structure[j-2] + "</a>";
+        }
+    }
+
+    marker.bindPopup(text);
+
+    marker.addTo(map);
+
+}
